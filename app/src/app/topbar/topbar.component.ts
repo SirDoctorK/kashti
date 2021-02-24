a9309465d1edcebe9eed70f1ce60d904a7dc5187
Cody Uhi <codylkuhi@gmail.com>
2021-02-24 16:52:13 -0700

    Installed and proved the SDK can interact with Brigade
    In this commit, I was able to import the SDK from NPM and use it to interact with a Brigade instance.

--- app/src/app/topbar/topbar.component.ts
+++ app/src/app/topbar/topbar.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import * as brigade from '@brigadecore/brigade-sdk';
 
 @Component({
   selector: 'app-topbar',
@@ -9,12 +10,30 @@ import { Component, OnInit } from '@angular/core';
 export class TopbarComponent implements OnInit {
 
   title = 'kashti';
+  apiAddress = ''; // Enter API address here
+  rootPassword = ''; // Enter Root Password here
+
   showDiv = {
     login: true,
   };
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  async getToken() {
+    console.log(this.rootPassword);
+    let client = new brigade.APIClient(this.apiAddress, '');
+    let token = await client
+      .authn()
+      .sessions()
+      .createRootSession(this.rootPassword)
+      .then((val) => {
+        console.log(val);
+      })
+      .catch((err) => {
+        console.error(err);
+      });
   }
 }
 
